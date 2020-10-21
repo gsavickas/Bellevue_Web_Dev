d98aff2d0eb34c2681fb8601b4eb2f4b9878c902
Grayton Savickas <gsavickaspro@gmail.com>
2020-10-21 15:59:14 -0500

    Adding comments and attribution to discussion 1.1

--- web231/Dis1-1.js
+++ web231/Dis1-1.js
@@ -1,6 +1,16 @@
+/*============================================
+; Title: Assignment 1.1
+; Author: Grayton Savickas
+; Date: 21 Oct 2020
+; Modified By: N/A
+; Description: This program demonstrates the; use of JavaScript types, values, and 
+; and variables in an application.
+;===========================================*/
+// variable to store the value of 45 
 let i = 45;
-let return = 30;
-
-console.log(🤓);
-
-x = i + 15;
+// variable to store the value of 15 
+let return = 15;
+// variable to hold the sum of i and return
+x = i + return;
+// print out the value of x
+console.log("x")
