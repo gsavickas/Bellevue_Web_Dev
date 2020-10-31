aa0bbb97dcdcf683f02528a7e79da6676ecb9ef3
Grayton Savickas <gsavickaspro@gmail.com>
2020-10-31 12:02:18 -0500

    Added assignment 2.2 Header

---
+++ web231/week-2/Savickas-header-2.2.js
@@ -0,0 +1,5 @@
+const header = require('../week-2/Savickas-header')
+
+// Tests the import of header function works
+var headerValue = header.display("Grayton", "Savickas", "Header")
+console.log(headerValue);
